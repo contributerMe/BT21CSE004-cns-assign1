e3aa72d83829a965efd1507742d241b46253f600
nimesh <nmaslekar777@gmail.com>
2024-02-22 08:24:42 +0530

    final1

--- q1.c
+++ q1.c
@@ -5,12 +5,21 @@
 void print_divisors(const mpz_t n) {
     mpz_t i;
     mpz_init(i);
+
+    int first = 1; // Flag to determine if it's the first divisor
+
     for (mpz_set_ui(i, 1); mpz_cmp(i, n) <= 0; mpz_add_ui(i, i, 1)) {
         // If 'n' is divisible by 'i', then 'i' is a divisor
         if (mpz_divisible_p(n, i)) {
-            gmp_printf("%Zd ", i);
+            if (!first) {
+                printf(" "); // Print space before each divisor (except the first one)
+            } else {
+                first = 0;
+            }
+            gmp_printf("%Zd", i);
         }
     }
+
     mpz_clear(i);
 }
 
